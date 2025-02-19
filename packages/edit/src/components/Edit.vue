b31901dac6c5c9087877f36a32650071666990e2
rkusan00 <renato.kusanovic@gmail.com>
2025-02-19 12:36:54 +0100

    Implement authoring package

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,42 +1,60 @@
 <template>
-  <div class="tce-container">
-    <div>This is Edit version of the content element id: {{ element?.id }}</div>
-    <div class="mt-6 mb-2">
-      Counter:
-      <span class="font-weight-bold">{{ element.data.count }}</span>
-    </div>
-    <button @click="increment">Increment</button>
+  <div class="tce-video">
+    <BrightcovePlayer
+      v-if="isConfigured"
+      ref="player"
+      :account-id="config.accountId!"
+      :player-id="config.playerId!"
+      :video-id="config.videoId!"
+      class="player"
+    />
+    <ElementPlaceholder
+      v-else
+      :icon="manifest.ui.icon"
+      :is-disabled="isDisabled"
+      :is-focused="isFocused"
+      :name="`${manifest.name} component`"
+      active-icon="mdi-arrow-up"
+      active-placeholder="Use toolbar to set the video parameters"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineEmits, defineProps } from 'vue';
-import { Element } from '@tailor-cms/ce-brightcove-video-manifest';
+import { computed, ref, watch } from 'vue';
+import type { Element } from '@tailor-cms/ce-brightcove-video-manifest';
+import { ElementPlaceholder } from '@tailor-cms/core-components';
+import manifest from '@tailor-cms/ce-brightcove-video-manifest';
 
-const emit = defineEmits(['save']);
-const props = defineProps<{ element: Element; isFocused: boolean }>();
+import BrightcovePlayer from './BrightcovePlayer.vue';
 
-const increment = () => {
-  const { data } = props.element;
-  const count = data.count + 1;
-  emit('save', { ...data, count });
-};
+const props = defineProps<{
+  element: Element;
+  isFocused: boolean;
+  isDisabled: boolean;
+}>();
+defineEmits(['save']);
+
+const player = ref<any>(null);
+const config = computed(() => props.element.data);
+const isConfigured = computed(
+  () => config.value.accountId && config.value.playerId && config.value.videoId,
+);
+
+watch(
+  () => props.isFocused,
+  (val) => {
+    if (!val && player.value) player.value.pause();
+  },
+);
 </script>
 
-<style scoped>
-.tce-container {
-  background-color: transparent;
-  margin-top: 1rem;
-  padding: 1.5rem;
-  border: 2px dashed #888;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 1rem;
-}
+<style lang="scss" scoped>
+.tce-video {
+  text-align: left;
 
-button {
-  margin: 1rem 0 0 0;
-  padding: 0.25rem 1rem;
-  background-color: #eee;
-  border: 1px solid #444;
+  iframe {
+    aspect-ratio: 16/9;
+  }
 }
 </style>
--- packages/edit/src/components/TopToolbar.vue
+++ packages/edit/src/components/TopToolbar.vue
@@ -1,14 +1,80 @@
 <template>
-  <div>
-    <p>Edit element top toolbar {{ element?.id }}</p>
-  </div>
+  <VForm ref="form" validate-on="submit" @submit.prevent="save">
+    <VToolbarItems class="ga-2">
+      <VTextField
+        v-model="elementData.accountId"
+        :disabled="!isEditing"
+        :rules="[rules.required]"
+        class="required mt-2 mr-3"
+        hide-details="auto"
+        label="Account Id"
+        min-width="150"
+        variant="outlined"
+      />
+      <VTextField
+        v-model="elementData.playerId"
+        :disabled="!isEditing"
+        :rules="[rules.required]"
+        class="required mt-2 mr-3"
+        hide-details="auto"
+        label="Player Id"
+        min-width="100"
+        variant="outlined"
+      />
+      <VTextField
+        v-model="elementData.videoId"
+        :disabled="!isEditing"
+        :rules="[rules.required]"
+        class="required mt-2 mr-3"
+        hide-details="auto"
+        label="Video Id"
+        min-width="150"
+        variant="outlined"
+      />
+      <VBtn v-if="!isEditing" @click="isEditing = true">Edit</VBtn>
+      <template v-else>
+        <VBtn v-if="isDirty" @click="save">Save</VBtn>
+        <VBtn v-if="isDirty || !isEmpty(elementData)" @click="cancel">
+          Cancel
+        </VBtn>
+      </template>
+    </VToolbarItems>
+  </VForm>
 </template>
 
-<script setup lang="ts">
-import { defineEmits, defineProps } from 'vue';
+<script lang="ts" setup>
+import { computed, reactive, ref } from 'vue';
+import type {
+  Element,
+  ElementData,
+} from '@tailor-cms/ce-brightcove-video-manifest';
+import cloneDeep from 'lodash/cloneDeep';
+import isEmpty from 'lodash/isEmpty';
+import isEqual from 'lodash/isEqual';
 
-defineProps<{ element: Element }>();
-defineEmits(['save']);
+const rules = {
+  required: (val: string) => !!val || 'The field is required.',
+};
+
+const props = defineProps<{ element: Element }>();
+const emit = defineEmits(['save']);
+
+const elementData = reactive<ElementData>(cloneDeep(props.element.data));
+const isEditing = ref(isEmpty(elementData));
+const form = ref<HTMLFormElement>();
+
+const isDirty = computed(() => !isEqual(elementData, props.element.data));
+
+const save = async () => {
+  const { valid } = await form.value?.validate();
+  if (!valid) return;
+  emit('save', elementData);
+  isEditing.value = false;
+};
+
+const cancel = () => {
+  Object.assign(elementData, cloneDeep(props.element.data));
+  form.value?.resetValidation();
+  isEditing.value = false;
+};
 </script>
-
-<style scoped></style>
